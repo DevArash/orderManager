9faaf8f0126f00ee19efda3fa89c70106228bcc2
Arash Kazemieh <arash.kazemieh@gmail.com>
2021-02-27 11:41:00 +0330

    center function finished
    unchecked

--- src/functions/center/funcs/getCenters.ts
+++ src/functions/center/funcs/getCenters.ts
@@ -1,6 +1,6 @@
 import { Bson } from "../../../../db.ts";
 import { centers, Center, RCenter } from "../../../schemas/mod.ts";
-import { makeLookUp } from "../../../utils/mod.ts";
+import { makeLookUp } from "../../../utils/makeLookUp.ts";
 import { makeProjections } from "../../../utils/makeProjections.ts";
 import { getMenus } from "../../menu/funcs/mod.ts";
 import { getTables } from "../../table/funcs/mod.ts";
@@ -8,7 +8,7 @@ import { getTables } from "../../table/funcs/mod.ts";
 type GetCentersInput = { filter: Bson.Document; getObj: RCenter };
 type GetCentersFn = ({ filter, getObj }: GetCentersInput) => Promise<Center[]>;
 export const getCenters: GetCentersFn = async ({ filter, getObj }) => {
-  const projection = makeProjections(getObj, [], ["menus", "tables"]);
+  const projection = makeProjections(getObj, [], ["tables", "menus"]);
   const foundedCenters = await centers.find(filter, { projection });
   let returnCenters = await foundedCenters.toArray();
   if (getObj.tables)
